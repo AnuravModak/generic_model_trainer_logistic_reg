ca62aa82c2c72cfb88240e5566fdf519370d79fc
Sourav Modak <72427070+official-srv-modak@users.noreply.github.com>
2024-06-03 13:24:44 -0400

    some changes and added url_prefix.

--- product_catalog_controller.py
+++ product_catalog_controller.py
@@ -1,32 +1,34 @@
 from flask import Flask, request, jsonify, Blueprint
 from flask_cors import CORS
-from flask_restful import Resource, Api
 from json_operations import get_product_list_json
 
+
+url_prefix = '/api'
 
 app = Flask(__name__)
 
 CORS(app)
 
-@app.route('/health', methods=['GET'])
-def health_check():
-    return 'Server is up and running!', 200
+# Define the blueprint
+product_bp = Blueprint('product_bp', __name__)
 
-@app.route('/train-model', methods=['POST'])
+@product_bp.route('/train-model', methods=['POST'])
 def chat_single():
     data = request.json
     print(data)
-
     return jsonify({'output': "output"}), 200
 
-@app.route('/get-all-products', methods=['GET'])
+@product_bp.route('/get-all-products', methods=['GET'])
 def get_all_products():
     products = get_product_list_json()
     return jsonify(products), 200
 
+# Register the blueprint with a URL prefix
+app.register_blueprint(product_bp, url_prefix=url_prefix)
 
-product_bp = Blueprint('product_bp', __name__)
-app.register_blueprint(product_bp, url_prefix='/api')
+@app.route('/health', methods=['GET'])
+def health_check():
+    return 'Server is up and running!', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
